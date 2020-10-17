7e205984e597cfc05d585180c471026fd642ca41
Br0n1k <Bron1ck337@gmail.com>
2020-10-17 16:30:25 +0300

    prepearings for cartpage

--- js/cart.js
+++ js/cart.js
@@ -16,13 +16,22 @@ window.onload = function () {
       };
       xhr.send();
    }
+
+   let goodsArray = {};
+   let cart = {};
    getJSON('https://spreadsheets.google.com/feeds/list/1PEfrN0lE3LlOaKiTs_2FoOVqlUeFPd2_R6iWbX8uHfI/od6/public/values?alt=json', function(err, data){
       if (err !== null){
          console.log('ERROR: ' + err);
       }
       else{
          data = data.feed.entry;
+         goodsArray = arrFilter(data);
          console.log(data);
+         console.log(goodsArray);
+         for (let x = 0; x < goodsArray.length; x++) {
+            let element = goodsArray[x];
+            console.log(element);
+         }
          document.querySelector('.shop-container').innerHTML = showGoods(data);
       }
    });
@@ -31,11 +40,42 @@ window.onload = function () {
       let itemData = '';
       for (i = 0; i < data.length; i++) {
          if(data[i].gsx$show.$t != 0){
-            itemData += '<div class="col-lg-3 col-md-6 text-center"><div class="goods"><h5>' + data[i].gsx$name.$t + '</h5><img src="' + data[i].gsx$img.$t + '" alt="img"><p class="cost">' + data[i].gsx$cost.$t + '</p><p class="amount"></p><p><button class="good-button btn btn-success" value="' + data[i].gsx$id.$t + '">Buy</button></p></div></div>'
+            itemData += '<div class="col-lg-3 col-md-6 text-center"><div class="goods"><h5>' + data[i].gsx$name.$t + '</h5><img src="' + data[i].gsx$img.$t + '" alt="img"><p class="cost">' + data[i].gsx$cost.$t + '</p><p class="amount"></p><p><button class="good-button btn btn-success" name="add-to-cart" value="' + data[i].gsx$id.$t + '">Buy</button></p></div></div>'
          }
       }
       return itemData;     
    }
+
+   function arrFilter(arr){
+      let out = {};
+      for (let i = 0; i < arr.length; i++) {
+         let temp = {};
+         temp['id'] = arr[i].gsx$id.$t;
+         temp['name'] = arr[i].gsx$name.$t;
+         temp['image'] = arr[i].gsx$img.$t;
+         temp['cost'] = arr[i].gsx$cost.$t;
+         out[temp['id']] = temp;
+      }
+      return out;
+   }
+
+   document.onclick = function(e){
+      if(e.target.name == 'add-to-cart'){
+         let itemId = e.target.value;
+         let itemPrice; // добавить в корзину короч все
+         console.log(e.target);
+      }
+   }
+
+
+
+
+
+
+
+
+
+
 
 
 
